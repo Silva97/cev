7e9e6a478ef79b40a143080aaabbc00dacc5f7da
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-16 12:14:44 -0300

    Tests to queue and stack.

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 make test name=lexer || exit
+make test name=parser || exit
 
 echo "* All tests OK *"
 exit 0
---
+++ tests/test_parser.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "cev.h"
+#include "metric.h"
+
+test_t test_queue(void)
+{
+  token_t *tk;
+  cev_queue_t *queue = NULL;
+
+  for (int i = 0; i < 10; i++) {
+    tk = calloc(1, sizeof *tk);
+    tk->type = TK_NUMBER;
+    tk->value = i;
+    queue_insert(&queue, tk);
+  }
+
+  for (int i = 0; (tk = queue_pop(&queue)); i++) {
+    printf("%" PRId64 ", ", tk->value);
+    METRIC_ASSERT(tk->value == i);
+  }
+  
+  putchar('\n');
+  queue_free(queue);
+
+  METRIC_TEST_OK("");
+}
+
+test_t test_stack(void)
+{
+  token_t *tk;
+  cev_stack_t *stack = NULL;
+
+  for (int i = 0; i < 10; i++) {
+    tk = calloc(1, sizeof *tk);
+    tk->type = TK_NUMBER;
+    tk->value = i;
+    stack_push(&stack, tk);
+  }
+
+  for (int i = 9; (tk = stack_pop(&stack)); i--) {
+    printf("%" PRId64 ", ", tk->value);
+    METRIC_ASSERT(tk->value == i);
+  }
+  
+  putchar('\n');
+  stack_free(stack);
+
+  METRIC_TEST_OK("");
+}
+
+int main(void)
+{
+  METRIC_TEST(test_queue);
+  METRIC_TEST(test_stack);
+
+  METRIC_TEST_END();
+  return metric_count_tests_fail;
+}
