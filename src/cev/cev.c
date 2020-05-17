e276daa888e81dd47332612e5f8d37815f5967b5
Luiz Felipe <felipe.silva337@yahoo.com>
2020-05-16 21:16:01 -0300

    Implements the cev() function.

---
+++ src/cev/cev.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include "cev.h"
+
+/**
+ * @brief Evaluate a expression using a context
+ * 
+ * The context has the variables' values.
+ * 
+ * @param ctx       If NULL, don't use a context.
+ * @param input     The expression to evaluate.
+ * @return int64_t  The result of the expression.
+ */
+int64_t cev(cev_t *ctx, char *input)
+{
+  cev_t *cev;
+  int64_t ret;
+
+  if (ctx)
+    cev = ctx;
+  else
+    cev = calloc(1, sizeof *cev);
+
+  token_t *tklist = cev_lexer(input);
+
+  if ( !cev_parser(cev, tklist) )
+    exit(EXIT_FAILURE);
+  
+  ret = cev_eval(cev);
+  if ( !ctx )
+    cev_free(cev);
+  
+  token_free(tklist);
+  return ret;
+}
